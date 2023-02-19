113ea555bf0109851d8e8e542795717688b34f02
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2023-02-19 14:38:19 -0600

    Create user-bank-model.js

---
+++ models/user-bank-model.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+
+
+const BankSchema = new Schema({
+  bank_name: { type: String, required: true },
+  account_number: { type: Number, required: true, minLength: 9, maxLength: 12 },
+  branch: { type: String, required: true },
+});
+
+
+
+module.exports = mongoose.model("Bank", BankSchema);
+
+
