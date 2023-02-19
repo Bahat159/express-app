828f0d994f95dcff3e966008d9850b9c0491d9f6
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2023-02-19 14:37:18 -0600

    Create user-sibling-model.js

---
+++ models/user-sibling-model.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+
+const UserSibling = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true},
+  sex: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Sibling", UserSibling);
