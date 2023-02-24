3e8514932df45343bb8254b11f791e3290248a59
Busari Habibullaah <34791977+Bahat159@users.noreply.github.com>
2023-02-24 12:56:42 -0600

    Update and rename user-bank-model.js to bank.js

--- models/bank.js
+++ models/bank.js
@@ -1,16 +1,11 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-
-
 const BankSchema = new Schema({
+  account_name: {type: String, require: true},
   bank_name: { type: String, required: true },
   account_number: { type: Number, required: true, minLength: 9, maxLength: 12 },
   branch: { type: String, required: true },
 });
 
-
-
 module.exports = mongoose.model("Bank", BankSchema);
-
-
